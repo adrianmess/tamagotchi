f3c9e9a1719ada122194c744935bea0337b89293
Guest User <Guest@Epicodus-9B.local>
2018-04-23 09:27:44 -0700

    create class file and add initial properties for Tamagotchi class

---
+++ src/tamagotchi.js
@@ -0,0 +1,21 @@
+class Tamagotchi {
+  constructor() {
+    this.name = "Gerald";
+    this.health = 100;
+    this.food = 0;
+    this.sleep = 0;
+    this.play = 0;
+    this.status = "";
+  }
+   setHealth() {
+     setInterval(() => {
+       this.health--;
+     }, 1000);
+    setFeed() {
+      
+    }
+     }
+   }
+}
+
+export {Tomagatchi}
